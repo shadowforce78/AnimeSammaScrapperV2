7b83102597e864768a4fa33006319078f94a2e9f
shadowforce78 <planque.adam@gmail.com>
2025-10-16 15:06:19 +0200

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.catalogue import return_data, fetch_all_catalogue_pages
+# from utils.catalogue import return_data, fetch_all_catalogue_pages
 from db.add_utils_to_db import add_utils_to_db
 from db.add_data_to_db import add_data_to_db
 from db.add_episodes_to_db import add_episodes_to_db
@@ -26,24 +26,27 @@ import json
 # episodes_data = add_episodes_to_db(all_episodes_data) # Scraped ✅
 
 # Scrap scan next
-# with open("data.json", "r", encoding="utf-8") as f:
-#     all_catalogue_items = json.load(f)
+with open("data.json", "r", encoding="utf-8") as f:
+    all_catalogue_items = json.load(f)
 
-# urls = []
-# # Get manga disponible for each item with manga
-# for item in all_catalogue_items:
-#     if item.get("details", {}).get("manga_disponible"):
-#         manga_disponible = item["details"]["manga_disponible"]
-#         for manga in manga_disponible:
-#             full_url = f"https://anime-sama.fr/catalogue/{item['title'].lower().replace(' ', '-').replace('#', '')}/{manga['url']}"
-#             urls.append(full_url)
+manga_by_title = {}
+# Get manga disponible for each item with manga
+for item in all_catalogue_items:
+    if item.get("details", {}).get("manga_disponible"):
+        manga_disponible = item["details"]["manga_disponible"]
+        title = item['title']
+        manga_by_title[title] = []
+        
+        for manga in manga_disponible:
+            full_url = f"https://anime-sama.fr/catalogue/{title.lower().replace(' ', '-').replace('#', '')}/{manga['url']}"
+            manga_by_title[title].append(full_url)
 
-# with open("manga_data.json", "w", encoding="utf-8") as f:
-#     # Put link in manga_data.json
-#     json.dump({"manga_links": urls}, f, ensure_ascii=False, indent=2)
+with open("manga_data.json", "w", encoding="utf-8") as f:
+    # Put links grouped by title in manga_data.json
+    json.dump(manga_by_title, f, ensure_ascii=False, indent=2)
 
 # Scrap all manga scans from manga_data.json
 # scrape_all_scans_from_manga_data()  # This will create scans_data.json
-with open("scans_data.json", "r", encoding="utf-8") as f:
-    all_scans_data = json.load(f)
-add_scans_to_db(all_scans_data)  # Scraped ✅
+# with open("scans_data.json", "r", encoding="utf-8") as f:
+#     all_scans_data = json.load(f)
+# add_scans_to_db(all_scans_data)  # Scraped ✅
